a44be06a2356116ae56d139acda904abef61f27a
Forest Bremer <forestb@ARSHIHIL5059195.local>
2016-08-17 13:44:44 -1000

    formatting whitespace

--- src/stats_manager.py
+++ src/stats_manager.py
@@ -5,26 +5,36 @@
 ### ok, update should take a list of seqs instead of a single seq
 ### then it can calculate stuff like longest seq, number of seqs, etc.
 
-class StatsManager:
+class StatsManager(object):
 
-    increment_stats = ["Total sequence length", "Number of genes", "Number of mRNAs", "Number of exons", "Number of introns", "Number of CDS",\
-            "Overlapping genes", "Contained genes", "CDS: complete", "CDS: start, no stop", "CDS: stop, no start", "CDS: no stop, no start",\
-            "Total gene length", "Total mRNA length", "Total exon length",\
-            "Total intron length", "Total CDS length"]    
-    min_stats = ["Shortest gene", "Shortest mRNA", "Shortest exon", "Shortest intron", "Shortest CDS"]
+    increment_stats = ["Total sequence length", "Number of genes",
+                       "Number of mRNAs", "Number of exons", "Number of introns", "Number of CDS",
+                       "Overlapping genes", "Contained genes", "CDS: complete",
+                       "CDS: start, no stop", "CDS: stop, no start", "CDS: no stop, no start",
+                       "Total gene length", "Total mRNA length", "Total exon length",
+                       "Total intron length", "Total CDS length"]
+
+    min_stats = ["Shortest gene", "Shortest mRNA", "Shortest exon", "Shortest intron",
+                 "Shortest CDS"]
+
     max_stats = ["Longest gene", "Longest mRNA", "Longest exon", "Longest intron", "Longest CDS"]
-    calc_stats_formulae = {"mean gene length": ["Total gene length", "Number of genes"],\
-            "mean mRNA length": ["Total mRNA length", "Number of mRNAs"],\
-            "mean exon length": ["Total exon length", "Number of exons"],\
-            "mean intron length": ["Total intron length", "Number of introns"],\
-            "mean CDS length": ["Total CDS length", "Number of CDS"],\
-            "% of genome covered by genes": ["Total gene length", "Total sequence length"],\
-            "% of genome covered by CDS": ["Total CDS length", "Total sequence length"],\
-            "mean mRNAs per gene": ["Number of mRNAs", "Number of genes"], "mean exons per mRNA": ["Number of exons", "Number of mRNAs"],\
-            "mean introns per mRNA": ["Number of introns", "Number of mRNAs"]}
-    calc_stats = ["mean gene length", "mean mRNA length", "mean exon length", "mean intron length",\
-            "mean CDS length", "% of genome covered by genes", "% of genome covered by CDS",\
-            "mean mRNAs per gene", "mean exons per mRNA", "mean introns per mRNA"]
+
+    calc_stats_formulae = {"mean gene length": ["Total gene length", "Number of genes"],
+                           "mean mRNA length": ["Total mRNA length", "Number of mRNAs"],
+                           "mean exon length": ["Total exon length", "Number of exons"],
+                           "mean intron length": ["Total intron length", "Number of introns"],
+                           "mean CDS length": ["Total CDS length", "Number of CDS"],
+                           "% of genome covered by genes": ["Total gene length",
+                                                            "Total sequence length"],
+                           "% of genome covered by CDS": ["Total CDS length",
+                                                          "Total sequence length"],
+                           "mean mRNAs per gene": ["Number of mRNAs", "Number of genes"],
+                           "mean exons per mRNA": ["Number of exons", "Number of mRNAs"],
+                           "mean introns per mRNA": ["Number of introns", "Number of mRNAs"]}
+
+    calc_stats = ["mean gene length", "mean mRNA length", "mean exon length", "mean intron length",
+                  "mean CDS length", "% of genome covered by genes", "% of genome covered by CDS",
+                  "mean mRNAs per gene", "mean exons per mRNA", "mean introns per mRNA"]
 
     def __init__(self):
         self.ref_stats = {}
@@ -32,9 +42,9 @@ class StatsManager:
         self.initialize_dict(self.ref_stats)
         self.initialize_dict(self.alt_stats)
 
-    def initialize_dict(self, d):
+    def initialize_dict(self, dictionary):
         for stat in self.increment_stats + self.min_stats + self.max_stats + self.calc_stats:
-            d[stat] = 0
+            dictionary[stat] = 0
 
     def alt_is_empty(self):
         for key in self.ref_stats.keys():
@@ -98,11 +108,13 @@ class StatsManager:
     def summary(self):
         for stat in self.calc_stats:
             self.calculate_stat(stat)
-        stats_order = [key for keys in [self.increment_stats, self.min_stats, self.max_stats, self.calc_stats] for key in keys]
+        stats_order = [key for keys in [self.increment_stats, self.min_stats, self.max_stats,
+                                        self.calc_stats] for key in keys]
         if self.alt_is_empty():
             return format_columns(["Genome"], stats_order, [self.ref_stats], 5)
         else:
-            return format_columns(["Reference Genome", "Modified Genome"], stats_order, [self.ref_stats, self.alt_stats], 5)
+            return format_columns(["Reference Genome", "Modified Genome"], stats_order,
+                                  [self.ref_stats, self.alt_stats], 5)
 
 ## UTILITY FUNCTIONS
 
@@ -116,22 +128,22 @@ def format_column(column, spacing):
     # Now format
     return [item+(' '*(longest-len(item))) for item in column]
 
-def format_columns(column_names, key_order, dicts, spacing = 3):
+def format_columns(column_names, key_order, dicts, spacing=3):
     # Build key column
     columns = [['', '']]
     columns[0].extend(key_order)
     columns[0] = format_column(columns[0], spacing)
-    
+
     # Notes: Python automatically sorts dictionary contents by key, so this will work.
-    # TODO: Nevertheless, make this code less hacky 
+    # TODO: Nevertheless, make this code less hacky
     # TODO: ^dictionary does not sort contents.  added sorted(dicts)
     #TODO: ^ my change broke a test, investigate
-    
+
     for i, dic in enumerate(dicts):
         new_column = [column_names[i], '-'*len(column_names[i])]
         new_column.extend([str(dic[key]) for key in key_order])
         columns.append(format_column(new_column, spacing))
-        
+
     # Finally, stringify the table
     tbl_str = ''
     for i in range(len(columns[0])): # For each row
