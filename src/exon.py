9f7f0e00156f005dcdbaeda64679576f17c054a0
Forest Bremer <forestb@ARSHIHIL5059195.local>
2016-08-17 12:47:11 -1000

    refactored exon.py using pylint
    refactored import *, broke up long line

--- src/exon.py
+++ src/exon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import copy
-from src.gene_part import *
+from src.gene_part import GenePart, write_tbl_entry
 
 class Exon(GenePart):
 
@@ -12,4 +12,5 @@ class Exon(GenePart):
     def to_tbl(self, has_start, has_stop, feature_type):
         """Returns a string representing the .tbl-formatted entry for this exon."""
         indices = copy.deepcopy(self.indices)
-        return write_tbl_entry(indices, self.strand, has_start, has_stop, feature_type, self.annotations)
+        return write_tbl_entry(indices, self.strand, has_start, has_stop,
+                               feature_type, self.annotations)
