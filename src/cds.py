ae156cb1a7b4ebae23537a798f0180c37db148f8
Forest Bremer <forest.bremer@gmail.com>
2016-08-20 10:50:21 -1000

    modified to use super()

--- src/cds.py
+++ src/cds.py
@@ -9,7 +9,7 @@ from src.translator import reverse_complement
 class CDS(GenePart):
     def __init__(self, identifier=None, indices=None,
                  score=None, phase=None, strand=None, parent_id=None):
-        GenePart.__init__(self, feature_type='CDS', identifier=identifier,
+        super(CDS, self).__init__(feature_type='CDS', identifier=identifier,
                           indices=indices, score=score, strand=strand, parent_id=parent_id)
         self.phase = []
         if phase is not None:
